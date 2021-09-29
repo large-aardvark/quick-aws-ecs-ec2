c7b5fce6940a41b440f7e35b284435ca4d7d645c
test <test@test.com>
2021-09-29 16:50:07 +0100

    Re-comment

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 ## Examples - uncomment to test
 ##
 
-module "simple-ecs-app2" {
-  source   = "./modules/ecs_ec2"
-  app_name = "myapp"
-  image_tag = "latest"
-  image_port = 3000
-}
+# module "simple-ecs-app2" {
+#   source   = "./modules/ecs_ec2"
+#   app_name = "myapp"
+#   image_tag = "latest"
+#   image_port = 3000
+# }
